5588de0f8027efb4b3650b640404a7e5af1320c2
Nikhil Kumar Sahu <75385653+nikhil822@users.noreply.github.com>
2023-06-06 21:29:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: 'http://127.0.0.1:5500',
+        origin: 'https://weather-app-five-tawny.vercel.app/',
     })
 )
 app.use(express.json());
