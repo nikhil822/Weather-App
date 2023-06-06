6552af3b3decfebb05486b73ba10307a7b35592d
Nikhil <nikhilsahu1312@gmail.com>
2023-06-06 20:54:50 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
-const http = require("http");
-const fs = require("fs");
-var requests = require("requests");
+require('dotenv').config()
+const express = require("express");
+const axios = require("axios");
+const cors = require('cors')
+const app = express();
 
-const homeFile = fs.readFileSync("index.html", "utf-8");
+app.use(
+    cors({
+        origin: 'http://127.0.0.1:5500',
+    })
+)
+app.use(express.json());
 
-const replaceVal = (tempVal, orgVal) => {
-    let temperature = tempVal.replace("{%tempval%}", Math.floor(orgVal.main.temp - 273));
-    temperature = temperature.replace("{%tempmin%}", Math.floor(orgVal.main.temp_min - 273));
-    temperature = temperature.replace("{%tempmax%}", Math.floor(orgVal.main.temp_max - 273));
-    temperature = temperature.replace("{%location%}", orgVal.name);
-    temperature = temperature.replace("{%country%}", orgVal.sys.country);
-    temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
-
-    return temperature;
-};
-
-const server = http.createServer((req, res) => {
-    if (req.url == "/") {
-        requests(
-        `https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid={API Key}`
-        )
-        .on("data", (chunk) => {
-            const objdata = JSON.parse(chunk);
-            const arrData = [objdata];
-            // console.log(arrData[0].main.temp);
-            const realTimeData = arrData.map((val) => replaceVal(homeFile, val)).join("");
-            res.write(realTimeData);
-            // console.log(realTimeData);
-        })
-        .on("end", (err) => {
-            if (err) return console.log("connection closed due to errors", err);
-            res.end();
-        });
-    } else {
-        res.end("File not found");
+// Define the POST endpoint for getting weather
+app.post("/getWeather", async (req, res) => {
+    try {
+        const { cities } = req.body;
+        const weatherData = await getWeatherData(cities);
+        res.json({ weather: weatherData });
+    } catch (error) {
+        console.error("Error:", error.message);
+        res.status(500).json({ error: "Failed to fetch weather data" });
     }
 });
 
-server.listen(8000, "127.0.0.1");
+// Helper function to fetch weather data for multiple cities
+async function getWeatherData(cities) {
+    // const apiKey = "YOUR_WEATHER_API_KEY"; // Replace with your weather API key
+    const weatherData = {};
+
+    // Make parallel API requests for each city
+    const requests = cities.map(async (city) => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${process.env.API_KEY}`;
+        const response = await axios.get(url);
+        // console.log(response.data.main.temp)
+        return { city, temperature: response.data.main.temp };
+    });
+
+    // Await all requests and populate weatherData object
+    const results = await Promise.all(requests);
+    results.forEach(({ city, temperature }) => {
+        weatherData[city] = `${temperature}C`;
+    });
+
+    return weatherData;
+}
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
